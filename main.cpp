014c7d559a386e1223350668468457e1d7e9f5b4
Víctor Manuel Rivas Santos <vrivas@ujaen.es>
2023-06-08 11:53:20 +0200

    Creo main.cpp
    Plantilla para main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+* @file main.cpp
+* @author Víctor Rivas <vrivas@ujaen.es>
+* @date 08-jun-2023
+* @desc Plantilla de fichero main.cpp
+*/
+#include <iostream>
+using namespace std;
+
+/// Función principal
+int main() {
+    cout<<"Hello World";
+    return 0;
+}
